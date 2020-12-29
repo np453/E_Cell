24b5c7dc6db3fbbebed82869faeb0e7ae48788c6
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2020-12-30 01:35:22 +0530

    updated carousel template

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Carousel} from 'react-bootstrap'
+import s1 from '../assets/s1.png'
 class Test extends Component {
   render() {
     return (
@@ -8,8 +9,8 @@ class Test extends Component {
         <Carousel>
   <Carousel.Item interval={1000}>
     <img
-      className="d-block w-100"
-      src=""
+      className="d-block img img-fluid"
+      src={s1}
       alt="First slide"
     />
     <div className="col-md-12">
@@ -23,25 +24,26 @@ class Test extends Component {
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src=""
-      alt="First slide"
-    />
-    <div className="col-md-12">
-      <div className="row">Bill Gates</div>
-      <div className="row">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
-      <div className="row">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+    <div className="container p-0">
+        <div className="row p-0 m-0">
+            <div className="col-md-6">
+                Image
+            </div>
+            <div className="col-md-6">
+            <img className=" w-100" src={s1} alt="First slide" />
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-12">
+                Image Section Body
+            </div>
+        </div>
     </div>
-    <Carousel.Caption style={{top:"0%"}}>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item interval={1000}>
     <img
       className="d-block w-100"
-      src=""
+      src={s1}
       alt="First slide"
     />
     <div className="col-md-12">

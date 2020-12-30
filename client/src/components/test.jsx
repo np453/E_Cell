48e2eb8312fd18affbb2af91755efda4bb62de39
Naman Patel <46571853+np453@users.noreply.github.com>
2020-12-30 14:04:23 +0530

    carousel final

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -4,58 +4,81 @@ import s1 from '../assets/s1.png'
 class Test extends Component {
   render() {
     return (
-      <div className="container-fluid p-5">
-          <div className="container carousel_container ">
+      <div className="container-fluid p-3">
+          <div className="container carousel_container" >
         <Carousel>
   <Carousel.Item interval={1000}>
-    <img
-      className="d-block img img-fluid"
-      src={s1}
-      alt="First slide"
-    />
-    <div className="col-md-12">
-      <div className="row">Bill Gates</div>
-      <div className="row">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
-      <div className="row">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+    
+    <div className="col-md-12 container">
+      <div className="row justify-content-center">
+          <img
+          className="d-block img img-fluid img_wrapper"
+          src={s1}
+          alt="First slide"
+          height="100"
+          width="100"
+        />
+      </div>
+      <div className="content_wrapper">
+        <div className="row justify-content-center mt-4">Bill Gates</div>
+      <div className="row justify-content-center">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
+      <div className="row justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      
     </div>
-    <Carousel.Caption style={{top:"0%"}}>
+    {/* <Carousel.Caption style={{top:"0%"}}>
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+    </Carousel.Caption> */}
   </Carousel.Item>
   <Carousel.Item interval={1000}>
-    <div className="container p-0">
-        <div className="row p-0 m-0">
-            <div className="col-md-6">
-                Image
-            </div>
-            <div className="col-md-6">
-            <img className=" w-100" src={s1} alt="First slide" />
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-md-12">
-                Image Section Body
-            </div>
-        </div>
+    
+    <div className="col-md-12 container">
+      <div className="row justify-content-center">
+          <img
+          className="d-block img img-fluid img_wrapper"
+          src={s1}
+          alt="First slide"
+          height="100"
+          width="100"
+        />
+      </div>
+      <div className="content_wrapper">
+        <div className="row justify-content-center mt-4">Bill Gates</div>
+      <div className="row justify-content-center">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
+      <div className="row justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      
     </div>
-  </Carousel.Item>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src={s1}
-      alt="First slide"
-    />
-    <div className="col-md-12">
-      <div className="row">Bill Gates</div>
-      <div className="row">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
-      <div className="row">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-    </div>
-    <Carousel.Caption style={{top:"0%"}}>
+    {/* <Carousel.Caption style={{top:"0%"}}>
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+    </Carousel.Caption> */}
   </Carousel.Item>
+  <Carousel.Item interval={1000}>
+    
+    <div className="col-md-12 container">
+      <div className="row justify-content-center">
+          <img
+          className="d-block img img-fluid img_wrapper"
+          src={s1}
+          alt="First slide"
+          height="100"
+          width="100"
+        />
+      </div>
+      <div className="content_wrapper">
+        <div className="row justify-content-center mt-4">Bill Gates</div>
+      <div className="row justify-content-center">Nulla vitae elit libero, a pharetra augue mollis interdum.</div>
+      <div className="row justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+      
+    </div>
+    {/* <Carousel.Caption style={{top:"0%"}}>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption> */}
+  </Carousel.Item>  
 </Carousel>
       </div>
       </div>

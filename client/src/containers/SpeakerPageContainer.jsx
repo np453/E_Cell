839bb3f85ade1761cd4e9a04069ce9b444e5714f
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-01-03 22:43:24 +0530

    updated speaker page card

---
+++ client/src/containers/SpeakerPageContainer.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Speaker from '../common/speakerPageCard';
+import s1 from '../assets/s1.png'
+class SpeakerPageContainer extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row m-0 d-flex justify-content-center">
+                        <Speaker borderColor="#1D2B5C" cardColor="#010220" name="Lokesh Kumar" image="" designation="co-founder LOCO" body="Sushil Kumar, the co-founder of LOCO - a real-time mobile quiz show where contestants get a chance to win real money.
+                 The app rolled out in early December of 2017 and has since had 15M+ downloads.
+                Sushil's inclination towards entrepreneurship stemmed right in college and led him to launch LOCO, 
+                after having worked for a popular education app Unacademy as an iOS and web developer.
+                Not only "/>
+                <Speaker borderColor="#1D2B5C" cardColor="#010220" name="Lokesh Kumar" image="" designation="co-founder LOCO" body="Sushil Kumar, the co-founder of LOCO - a real-time mobile quiz show where contestants get a chance to win real money.
+                 The app rolled out in early December of 2017 and has since had 15M+ downloads.
+                Sushil's inclination towards entrepreneurship stemmed right in college and led him to launch LOCO, 
+                after having worked for a popular education app Unacademy as an iOS and web developer.
+                Not only "/>
+                <Speaker borderColor="#1D2B5C" cardColor="#010220" name="Lokesh Kumar" image="" designation="co-founder LOCO" body="Sushil Kumar, the co-founder of LOCO - a real-time mobile quiz show where contestants get a chance to win real money.
+                 The app rolled out in early December of 2017 and has since had 15M+ downloads.
+                Sushil's inclination towards entrepreneurship stemmed right in college and led him to launch LOCO, 
+                after having worked for a popular education app Unacademy as an iOS and web developer.
+                Not only "/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SpeakerPageContainer;

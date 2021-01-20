32c950a104d3afbd1b8accdaab1fafb05c6cbd35
Naman Patel <46571853+np453@users.noreply.github.com>
2021-01-20 15:41:06 +0530

    cube_images

--- client/src/components/admin/admindashboard.jsx
+++ client/src/components/admin/admindashboard.jsx
@@ -78,7 +78,7 @@ class Dashboard extends Component {
     } 
 
     // Workshop photos on click handler
-    onClickHandlerSeminar = () => {
+    onClickHandlerGallery = () => {
         const data = new FormData() 
           data.append('file', this.state.data.file)
           const config = {
@@ -86,7 +86,7 @@ class Dashboard extends Component {
                   'content-type': 'multipart/form-data'
               }
           };
-        axios.post("/api/seminar", data, config)
+        axios.post("/api/gallery", data, config)
           .then(res => { // then print response status
             console.log('upload success')
           })
@@ -462,9 +462,9 @@ class Dashboard extends Component {
             {/* works api section ends */}
 
 
-            {/*Sponsor api form */}
+            {/*Gallery api form */}
             <div className="col border rounded m-2 p-3">
-                <h3 className="text-center">Seminar Photos api form</h3>
+                <h3 className="text-center">Gallery Photos api form</h3>
                 <div className="container">
                     <div className="row">
                     <div className="col-md-12">
@@ -476,17 +476,17 @@ class Dashboard extends Component {
                                         <path d="M27.9218 8.71697C26.373 2.54679 20.1156 -1.19951 13.9454 0.34935C9.12353 1.55979 5.62233 5.72723 5.26159 10.6855C1.83788 11.2501 -0.479912 14.4833 0.0846966 17.907C0.586607 20.9507 3.22398 23.1798 6.30874 23.1675H11.5445V21.0732H6.30874C3.99546 21.0732 2.12015 19.1979 2.12015 16.8846C2.12015 14.5714 3.99546 12.6961 6.30874 12.6961C6.88709 12.6961 7.35588 12.2273 7.35588 11.6489C7.35065 6.444 11.5659 2.22034 16.7708 2.21517C21.2764 2.21065 25.155 5.39594 26.0265 9.8164C26.1126 10.2578 26.4708 10.595 26.9166 10.6541C29.7793 11.0618 31.7695 13.713 31.3619 16.5757C30.9959 19.1463 28.8011 21.0603 26.2045 21.0732H22.0159V23.1675H26.2045C30.2528 23.1553 33.5246 19.8636 33.5123 15.8153C33.5021 12.4455 31.1955 9.51666 27.9218 8.71697Z" fill="#686868"/>
                                         <path d="M16.0362 12.9998L11.8477 17.1884L13.3241 18.6649L15.7326 16.2669V26.3091H17.8269V16.2669L20.2248 18.6649L21.7013 17.1884L17.5127 12.9998C17.1043 12.5938 16.4447 12.5938 16.0362 12.9998Z" fill="#686868"/>
                                     </svg>
-                                    <span className="m-2">Upload Seminar Photos</span>
+                                    <span className="m-2">Upload Gallery Photos</span>
                             </label>
                             <input name="file" id="file" type="file" className="form-control" onChange={this.handleRadio}/>
                         </div>  
-                        <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandlerSeminar}>Upload</button>
+                        <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandlerGallery}>Upload</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        {/* sponsor section ends */}
+        {/* Gallery section ends */}
 
 
         <div className="row m-0">
@@ -574,7 +574,7 @@ class Dashboard extends Component {
                         <li onClick={()=>this.handleclick_getapi("recent")} className="list-group-item dashboard_Link">Recent Notifications API</li>
                         <li onClick={()=>this.handleclick_getapi("contact")} className="list-group-item dashboard_Link"> Get Email subs</li>
                         <li onClick={()=>this.handleclick_getapi("works")} className="list-group-item dashboard_Link">Events Api</li>
-                        {/* <li onClick={()=>this.handleclick_getapi("seminar")} className="list-group-item dashboard_Link">Seminar API</li> */}
+                        <li onClick={()=>this.handleclick_getapi("gallery")} className="list-group-item dashboard_Link">Gallery API</li>
                         <li onClick={()=>this.handleclick_getapi("getintouch")} className="list-group-item dashboard_Link">Get in Touch API</li>
                     </ul>
                     <Link style={{ color:"black", textDecoration:"none" }} to="/">Back to Home</Link>
